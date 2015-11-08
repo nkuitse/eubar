58e69c69e9c753cad7c4df3687c757b75d185be2
Paul Hoffman <nkuitse@nkuitse.com>
2015-11-08 15:20:52 -0500

    fix header output

--- src/eubstat.c
+++ src/eubstat.c
@@ -20,22 +20,20 @@ int
 main(int argc, char **argv) {
     struct eub eub;
     struct eubfile file;
-    size_t opt_h = 0;
 
-    if (eub_init(&eub) != 0)
-        eub_die(&eub, "init failed\n");
+    eub_init(&eub);
     ARGBEGIN {
         default  : usage();
     } ARGEND;
     eub.ipath = stdin;
     eub.ometa = stdout;
-    if (eub_write_meta_header(&eub) != 0)
+    if (eub_write_header(&eub))
         exit(eub.err);
     while (eub_read_path(&eub, &file)) {
         if (eub_stat(&eub, &file) == 0)
             (void) eub_write_meta(&eub, &file);
     }
-    if (eub_write_meta_footer(&eub) != 0)
+    if (eub_write_meta_footer(&eub))
         exit(eub.err);
     return(0);
 }
